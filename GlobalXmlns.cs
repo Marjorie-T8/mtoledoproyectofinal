4542865ee87adb73936943f9f6915745bc333867
Marjorie Toledo <tu-email@ejemplo.com>
2025-11-27 20:34:00 -0500

    Agregado .gitignore y versión inicial

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "mtoledoproyectofinal")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "mtoledoproyectofinal.Pages")]
